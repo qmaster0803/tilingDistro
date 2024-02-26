7fea4c39bf335868638301caabaaef4d74c32a85
qmaster0803 <qmaster080305@gmail.com>
2024-02-26 16:11:34 +0300

    wip

--- main.py
+++ main.py
@@ -8,13 +8,28 @@
 
 import os
 import subprocess
+import logging
+import time
+
+VERSION = "0.1"
+
+def drawProgressbar(message, value, max_value):
+        print('\u001b[100D', end='') # jump to the first char in line
+        char_weight = max_value / 50
+        filled_count = round(value/char_weight)
+        empty_count = 50 - filled_count
+        print(message, '|'+('#'*filled_count)+(' '*empty_count)+'|', str(value)+'/'+str(max_value), flush=True, end='')
+
+# Setup logging
+logging.basicConfig(format="%(asctime)s [%(levelname)s]: %(message)s", level=logging.INFO, filename="tilingDistroInstall.log")
+logging.info("TilingDistro Installer v%s", VERSION)
 
 # Preparations. Check root permission.
 if(os.geteuid() != 0):
         print("This script must be run as root!")
         exit()
 else:
-        print("TilingDistro v0.1 install script.")
+        print("TilingDistro v"+VERSION+" install script.")
 
 # Step 1. Install basic software
 while(True):
@@ -24,8 +39,17 @@ while(True):
                 print("Aborted.")
                 exit()
 
-base_packages = ['sudo', 'xorg', 'bspwm', 'sxhkd', 'rofi', 'alacritty', 'ranger']
-try:
-        ret = subprocess.check_output(["apt-get", "install"] + base_packages, stderr=subprocess.STDOUT)
-except subprocess.CalledProcessError:
-        print('Sorry, an error occured. Full log can be found in "tilingDistroInstall.log". Please contact author at qmaster080305@gmail.com.')
+base_packages = ['test123', 'sudo', 'xorg', 'bspwm', 'sxhkd', 'rofi', 'alacritty', 'ranger', 'htop', 'zsh', 'build-essential', 'cmake', '']
+logging.info("Installing base packages...")
+drawProgressbar("Installing base packages...", 0, len(base_packages))
+for package in base_packages:
+        result = subprocess.run(["apt-get", "install", "-y", package], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        drawProgressbar("Installing base packages...", 1, len(base_packages))
+        logging.info("Installed %s", package)
+        if(result.returncode != 0):
+                logging.critical(result.stderr.decode('utf-8'))
+                print() # to reset progressbar
+                print('Sorry, an error occured. Full log can be found in "tilingDistroInstall.log". Please contact author at qmaster080305@gmail.com.')
+                exit()
+print() # to reset progressbar
+logging.info("Base packages installed.")
